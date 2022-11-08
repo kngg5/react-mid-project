da4eff251482489a6b7c3dba2a4d86f5bc535093
Yaseen <yasinmamatov12@gmail.com>
2022-11-08 18:54:40 +0600

    Files added

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import React,{useEffect, useState} from 'react';
+
+function App() {
+  
+  const [characterCount, setCharacterCount] = useState(0);
+ 
+  
+  return (
+    <div className="App">
+      <img src="https://st-1.akipress.org/st_all/bilim/9/1519540999.2.jpg" id="img-logo" style={{width: "400px",}}  alt='alatoo logo'/>
+      <h1>Enter input text! And get your text as barcode!!</h1>
+      <h2 id='output-text'>{characterCount}</h2>
+      <textarea onChange={(e) => setCharacterCount(e.target.value)} placeholder='Enter text'></textarea>
+      
+      
+    </div>
+  );
+}
+
+export default App;
